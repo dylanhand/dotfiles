5702e55e66b5ec0f387b9205c3189bed9f65458c
Dylan Hand <dylan.hand@gmail.com>
2023-05-16 21:57:04 +0200

    Add nvim config
    Missing lspsaga for now. Based heavily on https://github.com/josean-dev/dev-environment-files.

---
+++ .config/nvim/lua/dylanhand/core/keymaps.lua
@@ -0,0 +1,51 @@
+vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- mash homerow to escape
+keymap.set("i", "jk", "<ESC>")
+keymap.set("i", "kj", "<ESC>")
+
+-- easy save
+keymap.set("n", "<leader>w", ":w<CR>")
+
+-- easy underscores
+keymap.set("i", "uu", "_")
+
+-- clear highlighted search results
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+-- don't copy deleted character to register
+keymap.set("n", "x", '"_x')
+
+-- increment & decrement
+keymap.set("n", "<leader>+", "<C-a>")
+keymap.set("n", "<leader>-", "<C-x>")
+
+-- splits
+keymap.set("n", "<leader>sv", "<C-w>v")
+keymap.set("n", "<leader>sh", "<C-w>s")
+keymap.set("n", "<leader>se", "<C-w>=")
+keymap.set("n", "<leader>sx", ":close<CR>")
+
+-- tabs
+keymap.set("n", "<leader>to", ":tabnew<CR>")
+keymap.set("n", "<leader>tx", ":tabclose<CR>")
+keymap.set("n", "<leader>]", ":tabn<CR>")
+keymap.set("n", "<leader>[", ":tabp<CR>")
+
+
+-- plugin keymaps
+
+-- vim-maximizer
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
+
+-- nvim-tree
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+-- telescope
+keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<CR>")
+keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<CR>")
+keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<CR>")
+keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<CR>")
+keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<CR>")
