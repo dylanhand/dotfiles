02b945acd2cadc519f88ab43d8bd54baa65fa9d7
Dylan Hand <dylan.hand@gmail.com>
2023-05-29 15:42:04 +0200

    nvim: move to LazyVim

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,18 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("i", "kj", "<Esc>")
+
+vim.keymap.set("i", "uu", "_")
+
+--------------------
+-- PLUGIN KEYMAPS --
+--------------------
+
+-- Vim Tmux Navigator
+vim.keymap.set("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>")
+vim.keymap.set("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>")
+vim.keymap.set("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>")
+vim.keymap.set("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>")
--- .config/nvim/lua/dylanhand/core/colorscheme.lua
+++
@@ -1,4 +0,0 @@
-local status, _ = pcall(vim.cmd, "colorscheme nightfly")
-if not status then
-  print("Colorsheme not found")
-end
